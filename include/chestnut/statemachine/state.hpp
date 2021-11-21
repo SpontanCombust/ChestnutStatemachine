9a62d854ba506d5f148b044248e44aff165c38a4
SpontanCombust <cedro_p@o2.pl>
2021-11-21 22:20:02 +0100

    DOC state & statemachine

--- include/chestnut/statemachine/state.hpp
+++ include/chestnut/statemachine/state.hpp
@@ -3,21 +3,44 @@
 
 namespace chestnut::statemachine
 {
+    /**
+     * @brief Base generic abstract class used for creating state interface
+     * 
+     * @tparam StateNameType a type of states' name
+     * @tparam ParentStatemachine type of the statemachine that will house the state
+     * 
+     * @details
+     * ParentStatemachine type should be forward declared before the state interface.
+     * 
+     * StateNameType can be any type you'd want to identify your states by, like an enum, string or a regular int.
+     * The type must however be able to be used for std::unordered_map as a key,
+     * so if you have a custom type you would want to use a name, you need to implement a std::hash template specialization for it
+     */
     template< typename StateNameType, class ParentStatemachine >
     class IState
     {
     protected:
-        ParentStatemachine *parent;
+        ParentStatemachine *parent; /*Pointer to the statemachine that houses the state*/
 
     public:
         typedef ParentStatemachine* ParentStatemachinePtrType;
-        StateNameType name; // has to be set by inheriting state type
+        StateNameType name; /*Custom name for any state that may inherit from the interface*/
 
     public:
         IState( ParentStatemachine *parent_ );
         virtual ~IState() = default;
         
+        /**
+         * @brief A method called whenever statemachine enters this state
+         * 
+         * @param prevState name of the state statemachine swithed from
+         */
         virtual void onEnter( StateNameType prevState ) = 0;
+        /**
+         * @brief A method called whenever statemachine exits this state
+         * 
+         * @param nextState name of the state statemachine is swithing to
+         */
         virtual void onExit( StateNameType nextState ) = 0;
     };
     
--- include/chestnut/statemachine/statemachine.hpp
+++ include/chestnut/statemachine/statemachine.hpp
@@ -6,7 +6,18 @@
 
 namespace chestnut::statemachine
 {
-    // StateInterface must inherit from IState
+    /**
+     * @brief Base generic class for creating a statemachine
+     * 
+     * @tparam StateNameType a type of states' name
+     * @tparam StateInterface a base (interface) class from which all states inherit
+     * 
+     * @details
+     * StateInterface type must inherit from IState to work properly.
+     * It can have any number of virtual methods that proper states should implement in some form
+     * 
+     * @see IState
+     */
     template< typename StateNameType, typename StateInterface >
     class IStatemachine
     {
@@ -20,36 +31,67 @@ namespace chestnut::statemachine
         virtual ~IStatemachine();
 
 
-        // Adds states of given types to the statemachine
-        // States must inherit from StateInterface type
-        // DefaultState type is the state type the statemachine is initialized with and will always stay on the bottom of state stack
-        // onEnter for the default state is called with the name of the default state
-        //
-        // It is possible to pass in only default state type
-        // This method should be called only once and always before calling any other method
+        /**
+         * @brief Adds states of given types to the statemachine and initialize the default state
+         * 
+         * @details
+         * States must inherit from StateInterface type.
+         * DefaultState type is the state type the statemachine is initialized with and will always stay on the bottom of state stack.
+         * When finished, method calls onEnter for the default state with its own name
+         * It is possible to pass in only default state type.
+         * This method should be called only once and always before calling any other method.
+         * 
+         * @tparam DefaultState type of the default state for the statemachine
+         * @tparam States rest of the states
+         */
         template< class DefaultState, class ...States >
         void setupStates();
 
-
+        /**
+         * @brief Get the state object on top of the state stack
+         * 
+         * @return StateInterface* upcasted pointer to the state
+         */
         virtual StateInterface *getCurrentState() const;
 
+        /**
+         * @brief Get the name of the state object on top of the state stack
+         * 
+         * @return StateNameType name of the state
+         */
         virtual StateNameType getCurrentStateName() const;
 
-
-        // If state stack size is greater than 1, pops the state on the top of state stack and immediately pushes the specified state
-        // State transition happens directly, without consideration of a state below the top
-        //
-        // If state stack has only 1 element (the default state) the method acts like pushState()
-        // If state with that name was not supplied in the constructor or the same state is on top of the stack, method does nothing
+        /**
+         * @brief Transitions directly to specified state, forgetting its previous state afterwards
+         * 
+         * @details
+         * If state stack size is greater than 1, pops the state on the top of state stack and immediately pushes the specified state.
+         * State transition happens directly, without consideration of a state below the top.
+         * If state stack has only 1 element (the default state) the method acts like pushState().
+         * If state with that name was not supplied in the constructor or the same state is on top of the stack, method does nothing.
+         * 
+         * @param nextStateName name of the state statemachine should transition to
+         */
         virtual void gotoState( StateNameType nextStateName );
 
-        // Pushes next state onto the state stack
-        // State transition goes from state previously on top of the stack to the specified state
-        //
-        // If state with that name was not supplied in the constructor or the same state is on top of the stack, method does nothing
+        /**
+         * @brief Transitions to the specified state and rememebers its previous state afterwards
+         * 
+         * @details 
+         * Pushes next state onto the state stack.
+         * State transition goes from state previously on top of the stack to the specified state.
+         * If state with that name was not supplied in the constructor or the same state is on top of the stack, method does nothing.
+         * 
+         * @param nextStateName name of the state statemachine should transition to
+         */
         virtual void pushState( StateNameType nextStateName );
 
-        // Pops the state on top of the state stack unless there's only one state left (the default state)
+        /**
+         * @brief Transitions to the previous state
+         * 
+         * @details 
+         * Pops the state on top of the state stack unless there's only one state left (the default state).
+         */
         virtual void popState();
 
 
